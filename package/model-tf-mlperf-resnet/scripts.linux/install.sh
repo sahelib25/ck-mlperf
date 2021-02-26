8d9d8fe09906bd8acd52e8ac712cc645f96185be
Arjun <arjun@krai.ai>
2021-02-26 14:21:51 +0000

    Added resnet50 model fix

--- package/model-tf-mlperf-resnet/scripts.linux/install.sh
+++ package/model-tf-mlperf-resnet/scripts.linux/install.sh
@@ -5,7 +5,7 @@ echo "Fix input shape? ${fix_input_shape}"
 echo
 
 if [ "${fix_input_shape}" == "YES" ]; then
-  "${CK_ENV_COMPILER_PYTHON_FILE}" "${ORIGINAL_PACKAGE_DIR}/fix_input_shape.py" "${INSTALL_DIR}"
+  "${CK_ENV_COMPILER_PYTHON_FILE}" "${ORIGINAL_PACKAGE_DIR}/fix_input_shape.py" --input_graph resnet50_v1.pb --type b --output_graph resnet50_v1.pb
 fi
 
 echo "Done."
