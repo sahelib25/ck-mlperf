27dbdd213a0cffad73bb3eddd98d6593c078c131
Arjun <arjun@krai.ai>
2021-08-01 10:02:30 -0500

    Mobilenet pytorch model simplification

--- package/model-pytorch-mlperf-ssd-mobilenet/scripts.linux/install.sh
+++ package/model-pytorch-mlperf-ssd-mobilenet/scripts.linux/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 FILESUFFIX=""
-cp ${ORIGINAL_PACKAGE_DIR}/flatlabels.txt ${INSTALL_DIR}
 cp ${CK_ENV_MLPERF_INFERENCE_VISION}/"classification_and_detection/python/models/ssd_mobilenet_v1.py" ${INSTALL_DIR}/
 if [[ -n ${WITHOUT_ABP_NMS} ]]; then
   cp ${ORIGINAL_PACKAGE_DIR}/ssdMobileNetV1WithoutABPNMS.patch ${INSTALL_DIR}/
@@ -15,8 +14,9 @@ python3 -W ignore ssd_mobilenet_v1.py --input_model ./ssd_mobilenet_v1.pytorch
 echo "The SSDMobileNetV1_300_300${FILESUFFIX}.onnx is generated"
 
 if [[ -n ${SIMPLIFY_ONNX} ]]; then
-  cp ${ORIGINAL_PACKAGE_DIR}/splitAndSimplifyONNXGraph.py ${INSTALL_DIR}/
-  echo "Running the splitAndSimplifyONNXGraph.py Script to generate the simplified graph"
-  python3 -W ignore splitAndSimplifyONNXGraph.py --onnx_input "./SSDMobileNetV1_300_300${FILESUFFIX}.onnx" --onnx_output "./SSDMobileNetV1_300_300${FILESUFFIX}_batchsize_sim.onnx"
+  cp ${ORIGINAL_PACKAGE_DIR}/split_and_simplify.py ${INSTALL_DIR}/
+  echo "Running the split_and_simplify.py to remove NMS and generate the simplified graph"
+  echo "python3 -W ignore split_and_simplify.py ./SSDMobileNetV1_300_300${FILESUFFIX}.onnx"
+  python3 -W ignore split_and_simplify.py  "./SSDMobileNetV1_300_300${FILESUFFIX}.onnx"
 fi
 exit 0     
