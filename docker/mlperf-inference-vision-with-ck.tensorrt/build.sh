c56db50023ae7fe0b80a4881a788dffe0ed80252
elimkwan <elim@krai.ai>
2021-09-23 15:29:32 +0100

    fix build.sh after renaming directories

--- docker/mlperf-inference-vision-with-ck.tensorrt/build.sh
+++ docker/mlperf-inference-vision-with-ck.tensorrt/build.sh
@@ -3,21 +3,22 @@
 _BASE_IMAGE=${BASE_IMAGE:-nvcr.io/nvidia/tensorrt}
 _SDK_VER=${SDK_VER:-21.08-py3}
 _CK_VER=${CK_VER:-1.55.5}
+_TF_VER=${TF_VER:-2.6.0}
 _GROUP_ID=${GROUP_ID:-1500}
 _USER_ID=${USER_ID:-2000}
 
-TF_VER=tf-2.6.0
-CK_IMAGE=mlperf-inference-vision-with-ck.tensorrt.ubuntu-18.04
+CK_IMAGE=mlperf-inference-vision-with-ck.tensorrt
 
 read -d '' CMD <<END_OF_CMD
 cd $(ck find docker:${CK_IMAGE}) && \
-time docker build \
+time docker build --no-cache \
 --build-arg BASE_IMAGE=${_BASE_IMAGE} \
 --build-arg SDK_VER=${_SDK_VER} \
 --build-arg CK_VER=${_CK_VER} \
+--build-arg TF_VER=${_TF_VER} \
 --build-arg GROUP_ID=${_GROUP_ID} \
 --build-arg USER_ID=${_USER_ID} \
--t krai/${CK_IMAGE}:${_SDK_VER}_${TF_VER} \
+-t krai/${CK_IMAGE}:${_SDK_VER}_tf-${_TF_VER} \
 -f Dockerfile .
 END_OF_CMD
 echo "Running: ${CMD}"
